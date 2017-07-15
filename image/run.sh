7f7c52571fba93ff731b7ee328bf9650f7240367
AndreasMair <mair.andreas@gmail.com>
2017-07-15 22:31:44 +0200

    Delete run.sh

--- image/run.sh
+++
@@ -1,6 +0,0 @@
-convert -thumbnail 174 main_image1.jpg thumb1.jpg
-convert -thumbnail 174 main_image2.jpg thumb2.jpg
-convert -thumbnail 174 main_image3.jpg thumb3.jpg
-convert -thumbnail 1920 main_image1.jpg main_image1a.jpg
-convert -thumbnail 1920 main_image2.jpg main_image2a.jpg
-convert -thumbnail 1920 main_image3.jpg main_image3a.jpg
